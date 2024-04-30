283656f268027c046155b9c741acf65c82eeea02
serkankartal <51116499+serkankartal@users.noreply.github.com>
2024-04-30 15:27:59 +0300

    Update main.py
    added code snippet that makes face area green

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from Blur import *
 Folder4Blurring="./Data/images"
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    BlurFolder(Folder4Blurring)
+    BlurFolder(Folder4Blurring,blurring_size=50,make_facearea_green=1)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
